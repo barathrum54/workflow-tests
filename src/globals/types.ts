4ac73bd0e9bcfb6215ba7d30ebe1d37d7cb9d73b
barathrum54 <tahabdurmus0@gmail.com>
2023-05-15 15:25:48 +0300

    fix(fix12): 12
    12

--- src/globals/types.ts
+++ src/globals/types.ts
@@ -25,4 +25,4 @@ export type StateIcon = false | true
 // };
 // export type Icon = string | IconObject;
 // export type FullSize = "xs" | "sm" | "md" | "lg" | "xl" | "2xl";
-// Testing12
+// Testing1234
